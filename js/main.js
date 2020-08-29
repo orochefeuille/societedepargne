5885672895f4890c8f9f2ae7506e071807181a3c
orochefeuille <rochefeuille.o@gmail.com>
2020-08-29 13:33:28 +0200

    Create security file and display security layer

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// ********* Security layer on index.html **********
+
+// Get the "security.json" file
+httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = function() {
+    if(httpRequest.readyState === XMLHttpRequest.DONE) {
+        if(httpRequest.status === 200) {
+            let securityAdvice = JSON.parse(httpRequest.responseText);
+            createLayer(securityAdvice);
+
+        } else {
+            console.log("Une erreur est survenue !");
+        }
+    } else {
+        console.log("En attente de réponse");
+    }
+};
+ //Préparation et lancement de la requête
+ httpRequest.open('GET', 'doc/security.json', true);
+ httpRequest.send();
+
+ // Create DOM elements from "security.json" file for the security layer
+ function createLayer(file) {
+     const layer = document.getElementById("layer");
+     for(const key in file) {
+        if(key === "title") {
+            let layerTitle = document.createElement("h2");
+            layerTitle.classList.add("text-center", "font-weight-bold", "mt-5", "mb-5");
+            layerTitle.style.textDecoration = "underline";
+            layerTitle.innerText = file[key];
+            layer.appendChild(layerTitle);
+        }
+        if(key === "preamble") {
+            let layerPreamble = document.createElement("p");
+            layerPreamble.classList.add("m-5");
+            layerPreamble.innerText = file[key];
+            layer.appendChild(layerPreamble);
+        }
+        if(key === "advice") {
+            let layerAdviceUl = document.createElement("ul");
+            layerAdviceUl.classList.add("lead","list-unstyled","m-5");
+            for(const tip of file[key]) {
+                let layerAdviceLi = document.createElement("li");
+                layerAdviceLi.innerText = '* ' + tip;
+                layerAdviceLi.classList.add("m-3", "font-weight-bold");
+                layerAdviceUl.appendChild(layerAdviceLi);
+            }
+            layer.appendChild(layerAdviceUl);
+        }
+        if(key === "conclusion") {
+            let layerAdviceP = document.createElement("p");
+            layerAdviceP.innerText = file[key];
+            layerAdviceP.classList.add("lead", "mx-5", "text-info", "font-weight-bold", "bg-white", "p-3", "mb-5");
+            // layerTitle.style.textDecoration = "underline";
+            layer.appendChild(layerAdviceP);
+        }
+     }
+ }
