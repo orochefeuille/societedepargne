34d669ffcbbc345b2c753466e74aa920157c9b46
orochefeuille <rochefeuille.o@gmail.com>
2020-09-26 18:39:22 +0200

    Change json files to php and adapt pages

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@
 ?>
   <!-- Main -->
   <main class="container-fluid w-75 text-center">
-      <section class="statistics-wrapper">
+      <section class="blog-wrapper">
         <h2 class="text-info mb-5 lead font-weight-bold">Des articles spécialisés qui peuvent vous intéresser :</h2>
       </section> 
   </main>
@@ -12,3 +12,4 @@
 <?php
   require('footer.php');
 ?>
+<script src="js/blog.js"></script>
--- statistics.php
+++ statistics.php
@@ -1,17 +1,32 @@
 <?php
   require('navbar.php');
   require('header.php');
+
+  require('doc/savingsrate.php');
+  $rate = get_rate();
 ?>
 
   <main class="container-fluid w-75 text-center">
     <section class="statistics-wrapper">
       <h2 class="text-info mb-5">Restez informé :</h2>
-      <table class="table table-striped table-dark text-left col-lg-10 offset-lg-1 col-xl-6 offset-xl-3">
+      <table class="table table-striped table-dark text-left">
         <thead>
-          
+          <tr>
+            <?php foreach($rate[0] as $key => $item): ?>
+              <th scope="col"><?= $key; ?></th> 
+            <?php endforeach; ?>
+          </tr>
         </thead>
         <tbody>
-          
+          <tr>
+          <?php foreach($rate as $products): ?>
+            <tr scope="col">
+            <?php foreach($products as $product): ?>
+              <td><?= $product; ?></td>
+            <?php endforeach; ?>
+            </tr> 
+          <?php endforeach; ?>
+          </tr>
         </tbody>
       </table>
     </section>
