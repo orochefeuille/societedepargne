15e09fd01bf17d55e46571eddbc1955feb232efe
orochefeuille <rochefeuille.o@gmail.com>
2020-10-07 16:50:28 +0200

    Rebuild banque_php.sql

--- database/banque_php.sql
+++ database/banque_php.sql
@@ -1,14 +1,14 @@
 -- Delete existing database and user
-DROP DATABASE IF EXISTS banque_php;
-DROP USER IF EXISTS 'banquePHP'@'localhost';
+DROP DATABASE IF EXISTS ls_bank;
+DROP USER IF EXISTS 'ls_bank_user'@'localhost';
 
 -- Create database with utf-8 characters
-CREATE DATABASE banque_php CHARACTER SET 'utf8';
-USE banque_php;
+CREATE DATABASE ls_bank CHARACTER SET 'utf8';
+USE ls_bank;
 
 -- Create user and his privileges
-CREATE USER 'banquePHP'@'localhost' IDENTIFIED BY 'mdp';
-GRANT ALL ON banque_php.* TO 'banquePHP'@'localhost';
+CREATE USER 'ls_bank_user'@'localhost' IDENTIFIED BY 'mdp';
+GRANT ALL ON ls_bank.* TO 'ls_bank_user'@'localhost';
 
 -- Create banque_php tables
 
@@ -18,7 +18,7 @@ CREATE TABLE Customer (
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
     email VARCHAR(320) NOT NULL UNIQUE,    
-    pass VARCHAR(30) NOT NULL,
+    pass VARCHAR(320) NOT NULL,
     PRIMARY KEY(id)
 )
 ENGINE = InnoDB;
@@ -33,7 +33,7 @@ CREATE TABLE Account (
     id INT UNSIGNED AUTO_INCREMENT,
     label VARCHAR(30) NOT NULL,
     iban VARCHAR(30) NOT NULL UNIQUE,
-    date_creation DATE NOT NULL,    
+    date_creation TIMESTAMP NOT NULL,    
     balance DECIMAL(19,2) NOT NULL,
     customer_id INT UNSIGNED NOT NULL,
     PRIMARY KEY(id),
@@ -42,17 +42,17 @@ CREATE TABLE Account (
 ENGINE = InnoDB;
 -- Account table filling 
 INSERT INTO Account (label, iban, date_creation, balance, customer_id)
-VALUES  ('Compte courant', '3500 0004 5888 5666', '2015-11-15', -256.24, 1),
-        ('Livret A', '3500 0004 7474 9696', '2016-09-01', 569.96, 1),
-        ('Compte courant', '3500 0005 1221 4659', '2010-02-15', 1256.00, 2),
-        ('Plan Epargne Logement (PEL)', '3500 0005 7448 3942', '2019-05-27', 10248.69, 2),
-        ('Compte courant', '3500 0006 4566 9832', '2001-01-04', -50.01, 3),
-        ('Livret de développement durable (LDD)', '3500 0006 1618 3874', '2012-11-29', 21653.36, 3);
+VALUES  ('Compte courant', '3500 0004 5888 5666', '1447597943', -256.24, 1),
+        ('Livret A', '3500 0004 7474 9696', '1472804372', 569.96, 1),
+        ('Compte courant', '3500 0005 1221 4659', '1266231631', 1256.00, 2),
+        ('Plan Epargne Logement (PEL)', '3500 0005 7448 3942', '1558983479', 10248.69, 2),
+        ('Compte courant', '3500 0006 4566 9832', '978618730', -50.01, 3),
+        ('Livret de développement durable (LDD)', '3500 0006 1618 3874', '1354056600', 21653.36, 3);
 
 -- Transaction table creation 
 CREATE TABLE Transaction (
     id INT UNSIGNED AUTO_INCREMENT,
-    date_transaction DATETIME NOT NULL,
+    date_transaction TIMESTAMP NOT NULL,
     amount DECIMAL(19,2) NOT NULL,
     is_credit TINYINT(1) NOT NULL,    
     comments TEXT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE Transaction (
 ENGINE = InnoDB;
 -- Transaction table filling 
 INSERT INTO Transaction (date_transaction, amount, is_credit, comments, account_id)
-VALUES  ('2020-05-03 16:35:25', 150.00, 1, 'Joyeux anniversaire fillette', 1),
-        ('2020-04-26 09:15:22', 50.00, 0, '', 1),
-        ('2020-04-26 09:15:23', 50.00, 1, '', 2),
-        ('2020-10-03 22:55:59', 20.00, 0, 'Retrait DAB', 3);
+VALUES  ('1588496658', 150.00, 1, 'Joyeux anniversaire fillette', 1),
+        ('1587924730', 50.00, 0, '', 1),
+        ('1594548610', 50.00, 1, '', 2),
+        ('1601763556', 20.00, 0, 'Retrait DAB', 3);
