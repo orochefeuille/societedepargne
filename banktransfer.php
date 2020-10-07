513ffcafc5443116e532dbba1195f3de613fb1d2
orochefeuille <rochefeuille.o@gmail.com>
2020-10-07 14:50:06 +0200

    Add redirection to all pages

--- banktransfer.php
+++ banktransfer.php
@@ -1,7 +1,14 @@
 <?php
 $page_title ="Tranférer vers un autre compte | La Société d'épargne";
-  require('template/navbar.php');
-  require('template/header.php');
+
+session_start ();
+if (isset($_SESSION['cred']) != 'allowed' ) {
+  header('Location: http://localhost:8080/societedepargne/login.php');
+}
+
+require('template/navbar.php');
+require('template/header.php');
+
 ?>
   <main>
       <p>Virements</p>
--- login.php
+++ login.php
@@ -7,12 +7,12 @@
   // Get the credentials info
   function credentials_info(string $name) :string {
     $field = "";
-    if(isset($_POST['submit'])) {
+    if(isset($_POST['login'])) {
       $field = htmlspecialchars($_POST[$name]);
     }
     return $field;
   }
-  $pseudo = credentials_info("pseudo");
+  $email = credentials_info("email");
   $mdp = credentials_info("client-password");
 
   // Go to index.php if credentials are OK
@@ -20,24 +20,24 @@
       return ($p == "pseudo" && $m == "mdp") ? true : false;
   }
   
-  if (checkCredentials($pseudo, $mdp)):
+  if (checkCredentials($email, $mdp)):
     session_start();
     $_SESSION['cred'] = "allowed";
     header('Location: http://localhost/societedepargne/index.php');
-  elseif((checkCredentials($pseudo, $mdp) == false) && $pseudo && $mdp):?>
+  elseif((checkCredentials($email, $mdp) == false) && $email && $mdp):?>
     <div class="alert alert-danger w-75 mx-auto my-0 text-center">Les identifiants ne sont pas corrects</div>
   <?php endif; ?>
     <main class="mx-auto my-0">
       <form action="" method="post">
       <div class="form-group">
-          <label for="pseudo">Votre identifiant</label>
-          <input type="text" class="form-control" id="pseudo" name="pseudo" aria-describedby="pseudoHelp">
+          <label for="email">Votre adresse mail</label>
+          <input type="email" class="form-control" id="email" name="email" aria-describedby="pseudoHelp">
       </div>
       <div class="form-group">
           <label for="client-password">Mot de passe</label>
           <input type="password" class="form-control" id="client-password" name="client-password">
       </div>
-      <button type="submit" name="submit" class="btn bg-orange text-white">Se connecter</button>
+      <button type="submit" name="login" class="btn bg-orange text-white">Se connecter</button>
       </form>
   </main>
 
