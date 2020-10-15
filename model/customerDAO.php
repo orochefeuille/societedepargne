936c1da74dc01b39595d4bc202ecd18ae5f01f25
orochefeuille <rochefeuille.o@gmail.com>
2020-10-16 01:46:14 +0200

    Track untracked folders

---
+++ model/customerDAO.php
@@ -0,0 +1,31 @@
+<?php
+require "db_connection.php";
+
+class CustomerDAO {
+    private $db;
+    private $array;
+
+    public function __construct()
+    {
+        $dbConnexion = new DbConnection();
+        $this->db= $dbConnexion->getDb();
+    }
+
+    public function getCustomer(string $email) {
+        $query = $this->db->prepare(
+                    "SELECT * 
+                     FROM customer 
+                     WHERE email = :email
+                    "              
+        );
+        $result = $query->execute(
+            [
+                "email" => $email
+            ]
+        );
+
+        $customer= $query->fetch(PDO::FETCH_ASSOC);
+        return $customer;
+    }
+}
+
