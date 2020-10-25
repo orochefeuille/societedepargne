3550c1dd8e5a8f5068724f97b23881c3a99d9db4
orochefeuille <rochefeuille.o@gmail.com>
2020-10-25 11:57:56 +0100

    Create confirm card for transfer in baktransferView.php

--- view/bankTransferView.php
+++ view/bankTransferView.php
@@ -12,8 +12,8 @@ require('template/header.php');
                 <h2 class="text-info mb-5 lead font-weight-bold text-center">Effectuer un virement sécurisé :</h2>
                 <form class="container mt-2" action="" method="post" id="single-form">
                     <div class="form-group">
-                        <label for="accounts-list">Compte à débiter (obligatoire) :</label>
-                        <select class="form-control" id="accounts-list" name="label" required>
+                        <label for="accounts-list-debit">Compte à débiter (obligatoire) :</label>
+                        <select class="form-control" id="accounts-list-debit" name="accounts-list-debit" required>
                             <option value="" disabled selected >-- Choisir --</option>
                             <?php foreach($customer_accounts_labels as $customer_account_label): ?>
                                     <option><?= $customer_account_label ?> </option>
@@ -21,8 +21,8 @@ require('template/header.php');
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="accounts-list">Compte à créditer (obligatoire) :</label>
-                        <select class="form-control" id="accounts-list" name="label" required>
+                        <label for="accounts-list-credit">Compte à créditer (obligatoire) :</label>
+                        <select class="form-control" id="accounts-list-credit" name="accounts-list-credit" required>
                             <option value="" disabled selected >-- Choisir --</option>
                             <?php foreach($customer_accounts_labels as $customer_account_label): ?>
                                     <option><?= $customer_account_label ?> </option>
@@ -31,12 +31,12 @@ require('template/header.php');
                     </div>
                     <div class="form-group">
                         <label for="amount">Montant (obligatoire) :</label>
-                        <input class="w-100" type="number" value="valider" name="amount" min="0" step="0.01" required>
+                        <input class="w-100" type="number" value="valider" name="amount" id="transfer-amount" min="0" step="0.01" required>
                     </div>
                     <div class="form-group">
                         <button type="button" name="transfer_button" id="transfer_button" class="btn bg-orange text-white">Valider</button>
                     </div>
-                    <div id="transfer-card" class="card border-dark my-3">
+                    <div id="transfer-card" class="card border-dark my-3 opacity-0">
                         <div class="card-body bg-dark text-white">
                             <p class="card-text"> texte </p>
                             <button type="submit" name="confirm-transfer" id="confirm-transfer" class="btn bg-orange text-white font-weight-bold float-right">Confirmer</button>
@@ -49,6 +49,6 @@ require('template/header.php');
     </div>
 </main>
 <?php
-    $script=null;
+    $script="<script src='public/js/bankTransfer.js'></script>";
     require('template/footer.php');
 ?>
